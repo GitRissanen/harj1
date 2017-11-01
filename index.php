d2925c98830c3dcbaa4189b3a5aeedf1a5c2a237
Samuli <t5rijo00@students.oamk.fi>
2017-11-01 10:40:33 +0200

    Added HTML

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo 'Hello World';
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TestiSivu</title>
+  </head>
+  <body>
+    <?php
+    echo 'Hello World';
+    ?>
+  </body>
+</html>
