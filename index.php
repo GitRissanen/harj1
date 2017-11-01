908483272cc4927b683feb84fda5c15c0f793c20
Samuli <t5rijo00@students.oamk.fi>
2017-11-01 11:53:47 +0200

    PHP toimii

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <title>TestiSivu</title>
   </head>
   <body>
+    <h1>Assosiatiivinen array</h1>
     <?php
-    echo 'Hello World';
+    $nimet = array(array("Etunimi"=>'Ville',"Sukunimi"=>'Virta'),
+    array("Etunimi"=>'Kalle',"Sukunimi"=>'Järvi'),
+    array("Etunimi"=>'Liisa',"Sukunimi"=>'Virta'),
+    array("Etunimi"=>'Maija',"Sukunimi"=>'Joki')
+    );
     ?>
+    <h2>Koko taulukko</h2>
+    <table border="1">
+      <tr>
+        <th>Etunimi</th>
+        <th>Sukunimi</th>
+      </tr>
+      <?php
+      foreach ($nimet as $rivi) {
+        echo'<tr><td>'.$rivi["Etunimi"].'</td><td>'.$rivi["Sukunimi"].'</td></tr>';
+      }
+      ?>
+
+    </table>
   </body>
 </html>
