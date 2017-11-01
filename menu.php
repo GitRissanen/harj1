793d839305a4cd781385e1884cb6ed464dc96548
Samuli <t5rijo00@students.oamk.fi>
2017-11-01 12:00:53 +0200

    Added stuff

---
+++ menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TestiSivu</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="#">Etusivu</a></li>
+      <li><a href="eka.php">ArrayEsimerkki</a></li>
+      <li><a href="#">GetEsimerkki</a></li>
+      <li><a href="#">PostEsimerkki</a></li>
+    </ul>
+  </body>
+</html>
