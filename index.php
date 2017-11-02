be210b41c7b18860ad73fb2e0e2451d50dbb5cd2
Samuli <t5rijo00@students.oamk.fi>
2017-11-02 09:38:49 +0200

    Index.php added

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include "menu.php"; ?>
+<h1>PHPEsimerkki</h1>
+<p>Tämä on ensimmäinen esimerkki.</p>
+<?php include "footer.php";?>
--- menu.php
+++ menu.php
@@ -9,7 +9,7 @@
   <body>
     <div id="menu">
     <ul>
-      <li><a href="#">Etusivu</a></li>
+      <li><a href="index.php">Etusivu</a></li>
       <li><a href="eka.php">ArrayEsimerkki</a></li>
       <li><a href="getesim.php">GetEsimerkki</a></li>
       <li><a href="postesim.php">PostEsimerkki</a></li>
